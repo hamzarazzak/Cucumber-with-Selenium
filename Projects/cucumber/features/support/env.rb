1b3cefa1d3f12be94923587ea699bd8bcfc9dffa
hamzarazzak <hrazzak01@gmail.com>
2013-01-29 11:29:21 -0500

    commit changes 29/01

--- Projects/cucumber/features/support/env.rb
+++ Projects/cucumber/features/support/env.rb
@@ -17,7 +17,7 @@ Capybara.default_driver = :selenium
 Capybara.default_wait_time = 5
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
 
